3096cfdba1c6bcfba1f1517c581e6ea451d17c59
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-07 02:11:01 +0530

    included different methods for finding max
    included different methods for finding max for int, float and string array.

--- src/TestMaximumMain.java
+++ src/TestMaximumMain.java
@@ -10,15 +10,24 @@ import java.util.*;
 
 public class TestMaximumMain {
 
+	public static int getMaximum(Integer arrOfInt[]) {
+		return Stream.of(arrOfInt).max(Integer::compareTo).get();
+	}
+	public static float getMaximumFloat(Float arrOfFloat[]) {
+		return Stream.of(arrOfFloat).max(Float::compareTo).get();
+	}
+	public static String getMaximumString(String arrOfString[]) {
+		return Stream.of(arrOfString).max(String::compareTo).get();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Integer arrOfInt[] = new Integer[] {1,2,3};
 		Float arrOfFloat[] = new Float[] {1.25f,-2.14f,30f};
 		String arrOfString[] = new String[] {"Apple","Banana","Orange"};
-		Integer max = GenericMethod.getMaximum(arrOfInt);
-		Float maxF = GenericMethod.getMaximum(arrOfFloat);
-		String maxS = GenericMethod.getMaximum(arrOfString);
+		Integer max = getMaximum(arrOfInt);
+		Float maxF = getMaximumFloat(arrOfFloat);
+		String maxS = getMaximumString(arrOfString);
 		System.out.println(max);
 		System.out.println(maxF);
 		System.out.println(maxS);
