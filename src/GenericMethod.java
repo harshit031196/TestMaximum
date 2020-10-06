7fdeeb7c15f2d24b808a43ce0f7a8858a498db11
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-07 02:11:55 +0530

    Delete GenericMethod.java

--- src/GenericMethod.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-public class GenericMethod {
-	public static <T extends Comparable<T>> T getMaximum(T [] arr) {
-		return Arrays.stream(arr).collect(Collectors.maxBy(T::compareTo)).get();
-	}
-}
