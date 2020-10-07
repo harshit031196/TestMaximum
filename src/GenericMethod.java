a7e10aa95acb9c7a342aa3264cc098612e49660b
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-07 08:55:24 +0530

    Update GenericMethod.java

--- src/GenericMethod.java
+++ src/GenericMethod.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class GenericMethod {
-	public static <T extends Comparable<T>> T getMaximum(T [] arr) {
-		return Arrays.stream(arr).collect(Collectors.maxBy(T::compareTo)).get();
+	public static <T extends Comparable<T>> T getMaximum(T [] arrGeneric) {
+		return Arrays.stream(arrGeneric).collect(Collectors.maxBy(T::compareTo)).get();
 	}
 }
